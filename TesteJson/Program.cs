4e308314814acf7f66b4d70d650a7716093ab79a
hiagomenezes <hiago@itecnologia.com.br>
2019-10-08 16:28:21 -0300

    teste completo de um json

---
+++ TesteJson/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.IO;
+using Newtonsoft;
+using System.Collections.Generic;
+using System.Globalization;
+
+public class RPS
+{
+    public int id_dado { get; set; }
+    public int id_ies { get; set; }
+    public string nome_ies { get; set; }
+    public int id_filial { get; set; }
+    public string cod_filial { get; set; }
+    public string nome_filial { get; set; }
+    public int id_tipo_vinculo { get; set; }
+    public string descricao_tipo_vinculo { get; set; }
+    public object id_estado_civil { get; set; }
+    public object id_nacionalidade { get; set; }
+    public object id_tipo_documento { get; set; }
+    public object id_meio_transporte { get; set; }
+    public object id_raca { get; set; }
+    public int id_status_dados { get; set; }
+    public string descricao_status_dados { get; set; }
+    public string matricula { get; set; }
+    public string nome { get; set; }
+    public string nome_resumido { get; set; }
+    public object nome_mae { get; set; }
+    public string cpf_numero { get; set; }
+    public object flag_cpf_titular { get; set; }
+    public string documento_numero { get; set; }
+    public object documento_data_emissao { get; set; }
+    public object documento_orgao_expeditor { get; set; }
+    public object documento_uf { get; set; }
+    public DateTime data_nascimento { get; set; }
+    public string email { get; set; }
+    public string cod_barras { get; set; }
+    public object renda_mensal { get; set; }
+    public object numero_conta_corrente { get; set; }
+    public object ano_em_curso { get; set; }
+    public string data_termino_curso { get; set; }
+    public string curso { get; set; }
+    public object ano_conclusao_ensino_medio { get; set; }
+    public string sexo { get; set; }
+    public string campo_4 { get; set; }
+    public object numero_dependentes { get; set; }
+    public DateTime data_registro { get; set; }
+    public string alterado_por { get; set; }
+    public object percentual_preenchimento { get; set; }
+    public object flag_sincronizado { get; set; }
+    public DateTime data_atualizacao { get; set; }
+    public int id_usuario_atualizacao { get; set; }
+    public object data_inicio_curso { get; set; }
+    public object is_autoriza_contato { get; set; }
+    public string imagem_dado { get; set; }
+    public string imagem_ies { get; set; }
+    public string imagem_qrcode_foto { get; set; }
+    public string imagem_qrcode_acesso { get; set; }
+    public Telefone telefone { get; set; }
+    public Complemento complemento { get; set; }
+    public object endereco { get; set; }
+    public object cartao { get; set; }
+    public Foto foto { get; set; }
+    public Parametro parametro { get; set; }
+    public Movimentacao movimentacao { get; set; }
+    public Statuspedido[] statusPedido { get; set; }
+}
+
+public class Telefone
+{
+    public int id_dado_telefone { get; set; }
+    public int id_dados { get; set; }
+    public int id_tipo_telefone { get; set; }
+    public string ddd { get; set; }
+    public string numero { get; set; }
+    public object ramal { get; set; }
+}
+
+public class Complemento
+{
+    public int id_dado_complemento { get; set; }
+    public int id_dado { get; set; }
+    public object nome_social { get; set; }
+    public object nome_filial { get; set; }
+    public object periodo { get; set; }
+    public object cod_barras_2 { get; set; }
+    public object id_tipo_cartao { get; set; }
+    public object flag_correntista { get; set; }
+    public object data_registro { get; set; }
+    public object pagarme_customer_id { get; set; }
+}
+
+public class Foto
+{
+    public int id_dado_foto { get; set; }
+    public int id_dado { get; set; }
+    public int id_status_foto { get; set; }
+    public object id_motivo_rejeicao { get; set; }
+    public DateTime data_upload { get; set; }
+    public DateTime data_download { get; set; }
+    public object flag_foto_branca { get; set; }
+    public DateTime data_tratamento { get; set; }
+    public DateTime data_benft { get; set; }
+    public object descricao_motivo_rejeicao { get; set; }
+}
+
+public class Parametro
+{
+    public bool is_primeira_solicitacao { get; set; }
+    public bool solicita_isic { get; set; }
+    public string carteirinha_cor_fundo { get; set; }
+    public string carteirinha_cor_logo { get; set; }
+    public string carteirinha_cor_fonte { get; set; }
+    public object carteirinha_logo_filial { get; set; }
+    public bool is_acessa_foto { get; set; }
+    public bool is_acessa_dado { get; set; }
+}
+
+public class Movimentacao
+{
+    public int id_movimentacao { get; set; }
+    public int id_dado { get; set; }
+    public object id_movimentacao_tipo { get; set; }
+    public object id_lote { get; set; }
+    public int id_dado_historico { get; set; }
+    public int id_status_movimentacao { get; set; }
+    public object id_nsp { get; set; }
+    public object retorno { get; set; }
+    public DateTime data_solicitacao { get; set; }
+    public object data_producao { get; set; }
+    public string tipo_funcao_movimento { get; set; }
+    public bool flag_emissao { get; set; }
+    public object data_entrega_individual { get; set; }
+    public object id_usuario_entrega_individual { get; set; }
+    public int numero_sequencial_solicitacao { get; set; }
+    public object qr_code { get; set; }
+    public object id_dado_complemento_historico { get; set; }
+    public int id_tipo_cartao { get; set; }
+    public object credencial { get; set; }
+}
+
+public class Statuspedido
+{
+    public int status { get; set; }
+    public string mensagem { get; set; }
+    public DateTime data { get; set; }
+}
+
+
+namespace TesteJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///RPS teste = new RPS();
+            // esta linha é para pegar o caminho do json e ler linha por linha.
+           string fileJsonString =  File.ReadAllText(@"C:\temp\input\return-api.json");
+            // 
+            RPS teste = Newtonsoft.Json.JsonConvert.DeserializeObject<RPS>(fileJsonString);
+            Telefone teste2 = Newtonsoft.Json.JsonConvert.DeserializeObject<Telefone>(fileJsonString);
+            //Console.WriteLine(fileJsonString);
+
+            // o caminho destinho para a onde o arquivo vai depois de formatado
+            string output = string.Format(@"C:\temp\output\.txt", DateTime.Now);
+
+            using (StreamWriter pastadestino = new StreamWriter(fileJsonString))
+            {
+                pastadestino.WriteLine(fileJsonString);
+                pastadestino.WriteLine(teste.documento_orgao_expeditor + teste2.numero);
+                //pastadestino.WriteLine();
+
+                pastadestino.Flush();
+                pastadestino.Close();
+            }
+            Statuspedido kleber = new Statuspedido();
+            kleber.mensagem = "";
+
+            List<string> jsonlist = new List<string>();
+           
+            
+            
+
+
+
+        }
+
+        //public int id_dado { get; set; }
+        //public int id_ies { get; set; }
+        //public string nome_ies { get; set; }
+        //public string nome { get; set; }
+        //public string nome_mae { get; set; }
+
+    }
+}
---
+++ TesteJson/testelista.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteJson
+{
+    class testelista
+    {
+        /// <summary>
+        ///     Série do RPS
+        /// </summary>
+        public string serieRPS { get; set; }
+        /// <summary>
+        /// Série do NF-e. * Obrigatório somente para contribuintes com regime especial.
+        /// </summary>
+        public string serieNFe { get; set; }
+        /// <summary>
+        /// Número do RPS, iniciar no número 1, com zeros a esquerda, sendo que
+        ///obrigatoriamente os 3 primeiros dígitos sejam zero.Exemplo:
+        /// 0000000001. * Obrigatório somente para RPS com situação "E", para
+        /// RPS com situação "C" enviar zeros exceto para Código do Motivo de
+        ///Cancelamento 03 (Substituição), neste caso informar o número do novo
+        ///RPS.Observação os 3 primeiros dígitos são reservados. 
+        /// </summary>
+        public string numeroRPS { get; set; }
+        /// <summary>
+        /// Data de Emissão do RPS AAAAMMDD
+        /// Hora de Emissão do RPS HHMMSS
+        /// </summary>
+        public DateTime dataRPS { get; set; }
+        /// <summary>
+        /// E para RPS Enviado / C para RPS Cancelado
+        /// </summary>
+        public string situacaoRPS { get; set; }
+        /// <summary>
+        /// 01 para Cancelamento do Serviço / 02 para Dados Incorretos / 03 para
+        /// Substituição. * Obrigatório somente para os casos de RPS com Situação
+        ///   "C" (cancelado).
+        /// </summary>
+        public string codigoMotivoCancelamento { get; set; }
+        /// <summary>
+        /// Número da NF-e a ser cancelada/substituida. * Obrigatório somente
+        /// para os casos de RPS com Situação "C" (cancelado).
+        /// </summary>
+        public int numeroNFesubs { get; set; }
+        /// <summary>
+        /// Série da NF-e a ser cancelada/substituida. * Obrigatório somente para
+        ///     os casos de RPS com Situação "C" (cancelado) e para contribuintes com
+        ///regime especial.
+        /// </summary>
+        public string sNFeCS { get; set; }
+        /// <summary>
+        /// Data de emissão da NF-e a ser cancelada/substituida. * Obrigatório
+        /// somente para os casos de RPS com Situação "C" (cancelado).
+        /// </summary>
+        public DateTime nFeCS { get; set; }
+        /// <summary>
+        /// Descrição do Motivo de Cancelamento. * Obrigatório somente para os
+        /// casos de RPS com Situação "C" (cancelado).
+        /// </summary>
+        public string descricaoCancelamento { get; set; }
+        /// <summary>
+        /// Código da Atividade do Serviço Prestado
+        /// </summary>
+        public string cSP { get; set; }
+        /// <summary>
+        /// 1 para serviço prestado no Município / 2 para serviço prestado fora do
+        ///Município. * Obrigatório para as atividades relacionadas nas exceções,
+        ///conforme artigo 39 da LC 118/02.
+        /// </summary>
+        public string lPS { get; set; }
+        /// <summary>
+        /// 1 para serviço prestado em vias públicas / 2 para serviço não prestado
+        ///em vias públicas. * Obrigatório para as atividades dos itens 7.02 e 7.05
+        /// da lista de serviços, não prestados em vias públicas.
+        /// </summary>
+        public string sPVP { get; set; }
+        /// <summary>
+        /// Obrigatório para as atividades dos itens 7.02 e 7.05 da lista de
+        /// serviços, não prestados em vias públicas.
+        /// </summary>
+        public string enderecoL { get; set; }
+        /// <summary>
+        /// Obrigatório para as atividades dos itens 7.02 e 7.05 da lista de
+        /// serviços, não prestados em vias públicas.
+        /// </summary>
+        public string numeroL { get; set; }
+        /// <summary>
+        /// Obrigatório para as atividades dos itens 7.02 e 7.05 da lista de
+        /// serviços, não prestados em vias públicas.
+        /// </summary>
+        public string complementoL { get; set; }
+        /// <summary>
+        /// Obrigatório para as atividades dos itens 7.02 e 7.05 da lista de
+        /// serviços, não prestados em vias públicas.
+        /// </summary>
+        public string bairroL { get; set; }
+        /// <summary>
+        /// Obrigatório para as atividades relacionadas nas exceções, conforme
+        /// artigo 39 da LC 118/02
+        /// </summary>
+        public string cidadeL { get; set; }
+        /// <summary>
+        /// Obrigatório para as atividades relacionadas nas exceções, conforme
+        /// artigo 39 da LC 118/02.
+        /// </summary>
+        public string uFL { get; set; }
+        /// <summary>
+        /// Obrigatório para as atividades dos intes 7.02 e 7.05 da lista de
+        /// serviços, não prestados em vias públicas.
+        /// </summary>
+        public string cEPL { get; set; }
+        /// <summary>
+        /// Quantidade, somente valor inteiro.
+        /// </summary>
+        public string quantidadeServico { get; set; }
+        /// <summary>
+        /// Valor do Serviço, referente ao valor unitário, valor com 15 posições com
+        /// zeros a esquerda, sem vírgula e sem ponto.Exemplo: R$10,25 =
+        /// 000000000001025
+        /// </summary>
+        public string valorServico { get; set; }
+        /// <summary>
+        /// Somatória das Retenções, valor com 15 posições com zeros a esquerda,
+        /// sem vírgula e sem ponto.Exemplo: R$10,25 = 000000000001025
+        /// </summary>
+        public string valorTRetençoes { get; set; }
+        /// <summary>
+        /// 1 Para Tomador Estrangeiro 2 para Tomador Brasileiro
+        /// </summary>
+        public string tomadorEstrangeiro { get; set; }
+        /// <summary>
+        /// Códido do pais de nacionalidade do tomador, conforme tabela de paises,
+        /// quando o tomador for estrangeiro. * Exceto para tomadores brasileiros.
+        /// </summary>
+        public string pNTE { get; set; }
+        /// <summary>
+        /// 1 Para Serviço exportado 2 para Serviço não exportado. *Exceto para
+        /// tomadores brasileiros.
+        /// </summary>
+        public string sPE { get; set; }
+        /// <summary>
+        /// 1 para CPF / 2 para CNPJ * Exceto para Tomador estrangeiro
+        /// </summary>
+        public string identificacaoTomador { get; set; }
+        /// <summary>
+        /// Número do Documento do Tomador * Exceto para Tomador estrangeiro
+        /// </summary>
+        public string tCNPJ { get; set; }
+        /// <summary>
+        /// Razão Social / Nome do Tomador
+        /// </summary>
+        public string razaoSocial { get; set; }
+        /// <summary>
+        /// * Exceto para Tomador estrangeiro
+        /// </summary>
+        public string enderecoT { get; set; }
+        /// <summary>
+        /// * Exceto para Tomador estrangeiro
+        /// </summary>
+        public string numeroT { get; set; }
+        /// <summary>
+        /// * Exceto para Tomador estrangeiro
+        /// </summary>
+        public string complementoT { get; set; }
+        /// <summary>
+        /// * Exceto para Tomador estrangeiro
+        /// </summary>
+        public string bairroT { get; set; }
+        /// <summary>
+        /// * Exceto para Tomador estrangeiro
+        /// </summary>
+        public string cidadeT { get; set; }
+        /// <summary>
+        /// * Exceto para Tomador estrangeiro
+        /// </summary>
+        public string uFT { get; set; }
+        /// <summary>
+        /// * Exceto para Tomador estrangeiro
+        /// </summary>
+        public string cEPT { get; set; }
+        /// <summary>
+        /// e-mail do Tomador, permitido no máximo 3 e-mails, usando o caracter
+        /// "|" (pipe ou barra vertical) como delimitador de e-mail. * Obrigatório
+        /// somente para Tomador Pessoa Juridica
+        /// </summary>
+        public string email { get; set; }
+        /// <summary>
+        /// Número da Fatura
+        /// </summary>
+        public int nfatura { get; set; }
+        /// <summary>
+        /// Somatória das Retenções, valor com 15 posições com zeros a esquerda,
+        /// sem vírgula e sem ponto.Exemplo: R$10,25 = 000000000001025
+        /// </summary>
+        public int vfatura { get; set; }
+        /// <summary>
+        /// Forma de pagamento da Fatura.* Obigatório quando for informado o
+        /// número da fatura.
+        /// </summary>
+        public string formaPagamento { get; set; }
+        /// <summary>
+        /// Descritivo do Serviço Prestado (Este campo será impresso em um
+        /// retângulo com até 100 caracteres por linha(largura) e 13 linhas
+        /// (altura). Usar o caracter "|" (pipe ou barra vertical) como delimitador de
+        /// quebra de linha.A cada 100 caracteres é obrigatório o caracter de
+        /// quebra "|", não deverá ser colocado esse delimitador na 13º linha
+        /// (última linha).
+        /// </summary>
+        public string discriminacaoServico { get; set; }
+        /// <summary>
+        /// 3 *Obrigatório para lançamento de Retenção ou Lançamento de Valor
+        /// não Incluso na Base de Cálculo de Serviço.Este registro deverá repetir
+        /// para cada código conforme campo "Código de Outros Valores".
+        /// </summary>
+        public int tipoRegistro { get; set; }
+        /// <summary>
+        /// 01 - para IRRF
+        /// 02 - para PIS/PASEP
+        /// 03 - para COFINS
+        /// 04 - para CSLL
+        /// VN - para Valor não Incluso na Base de Cálculo(exceto tributos
+        /// federais). Esse campo somado ao valor total dos serviços resulta no
+        /// Valor total da nota.
+        /// </summary>
+        public string codOutVal { get; set; }
+        /// <summary>
+        /// Somatória das Retenções, valor com 15 posições com zeros a esquerda,
+        /// sem vírgula e sem ponto.Exemplo: R$10,25 = 000000000001025
+        /// </summary>
+        public string valor { get; set; }
+        /// <summary>
+        /// Valor Total dos Serviços contidos no Arquivo (Soma dos valores
+        /// unitários dos serviços multiplicados pelas quantidades de cada serviço)
+        /// </summary>
+        public int valToSer { get; set; }
+        /// <summary>
+        /// Valor Total das Retenções e outros valores informados no registro 3
+        /// </summary>
+        public int valToValReg { get; set; }
+        /// <summary>
+        /// 01 para Cancelamento do Serviço / 02 para Dados Incorretos / 03 para
+        /// Substituição. * Obrigatório somente para os casos de RPS com Situação
+        /// "C" (cancelado).
+        /// </summary>
+        public string codMovCancelamento { get; set; }
+
+        public testelista(string serieRPS, string serieNFe, string numeroRPS, DateTime dataRPS, string situacaoRPS, string codigoMotivoCancelamento, int numeroNFesubs, string sNFeCS, DateTime nFeCS, string descricaoCancelamento, string cSP, string lPS, string sPVP, string enderecoL, string numeroL, string complementoL, string bairroL, string cidadeL, string uFL, string cEPL, string quantidadeServico, string valorServico, string valorTRetençoes, string tomadorEstrangeiro, string pNTE, string sPE, string identificacaoTomador, string tCNPJ, string razaoSocial, string enderecoT, string numeroT, string complementoT, string bairroT, string cidadeT, string uFT, string cEPT, string email, int nfatura, int vfatura, string formaPagamento, string discriminacaoServico, int tipoRegistro, string codOutVal, string valor, int valToSer, int valToValReg, string codMovCancelamento)
+        {
+            this.serieRPS = serieRPS;
+            this.serieNFe = serieNFe;
+            this.numeroRPS = numeroRPS;
+            this.dataRPS = dataRPS;
+            this.situacaoRPS = situacaoRPS;
+            this.codigoMotivoCancelamento = codigoMotivoCancelamento;
+            this.numeroNFesubs = numeroNFesubs;
+            this.sNFeCS = sNFeCS;
+            this.nFeCS = nFeCS;
+            this.descricaoCancelamento = descricaoCancelamento;
+            this.cSP = cSP;
+            this.lPS = lPS;
+            this.sPVP = sPVP;
+            this.enderecoL = enderecoL;
+            this.numeroL = numeroL;
+            this.complementoL = complementoL;
+            this.bairroL = bairroL;
+            this.cidadeL = cidadeL;
+            this.uFL = uFL;
+            this.cEPL = cEPL;
+            this.quantidadeServico = quantidadeServico;
+            this.valorServico = valorServico;
+            this.valorTRetençoes = valorTRetençoes;
+            this.tomadorEstrangeiro = tomadorEstrangeiro;
+            this.pNTE = pNTE;
+            this.sPE = sPE;
+            this.identificacaoTomador = identificacaoTomador;
+            this.tCNPJ = tCNPJ;
+            this.razaoSocial = razaoSocial;
+            this.enderecoT = enderecoT;
+            this.numeroT = numeroT;
+            this.complementoT = complementoT;
+            this.bairroT = bairroT;
+            this.cidadeT = cidadeT;
+            this.uFT = uFT;
+            this.cEPT = cEPT;
+            this.email = email;
+            this.nfatura = nfatura;
+            this.vfatura = vfatura;
+            this.formaPagamento = formaPagamento;
+            this.discriminacaoServico = discriminacaoServico;
+            this.tipoRegistro = tipoRegistro;
+            this.codOutVal = codOutVal;
+            this.valor = valor;
+            this.valToSer = valToSer;
+            this.valToValReg = valToValReg;
+            this.codMovCancelamento = codMovCancelamento;
+        }
+
+    }
+}
